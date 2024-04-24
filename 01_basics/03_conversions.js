2269d2c8338c91e6d1c43c6fb9b4a55acf674d73
Vaibhav-123vas <126691958+Vaibhav-123vas@users.noreply.github.com>
2024-04-24 14:51:33 +0000

    container config done

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,16 @@
+let score =undefined;
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+let valueInNumber = Number(score)
+
+//console.log(valueInNumber)
+// "33"=>33
+//"33abc"=>NaN
+//false=>0 , true=>1
+ let isLoggedIn="vaibhav";
+ let BooleanIsLoggedIn=Boolean(isLoggedIn)
+ console.log(BooleanIsLoggedIn);
+
+ 
